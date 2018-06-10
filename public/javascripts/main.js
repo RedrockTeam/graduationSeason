cb776029808f8eb7a742cab691a05b44bf0b70bf
psx980626 <375180728@qq.com>
2018-06-11 00:42:09 +0800

    ..

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,44 +3,58 @@ import loadImg from './loadImg.js'
 import loadUsername from './loadUsername.js'
 
 
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext('2d');
-//    瓦片canvas
-var tCanvas = document.createElement("canvas");
-var tctx = tCanvas.getContext("2d");
-var input = document.getElementById("username");
-input.scrollIntoViewIfNeeded();
+
+$('.members').click(function(event) {
+    $('.black').css({
+        backgroundColor: 'rgba(0,0,0,0.7)',
+        zIndex: '10000'
+    });
+    $('.membersContainer').css({
+        display: 'block'
+    });
+});
+
+$('.close').click(function(event) {
+    $('.membersContainer').css({
+        display: 'none'
+    });
+    $('.black').css({
+        backgroundColor: 'rgba(0,0,0,0)',
+        zIndex: '-1'
+    });
+});
 
 
-function getUserinfo() {
-    var myUrl = encodeURI('https://wx.idsbllp.cn/nodejs/graduationSeason/');
-    var apiUrl = 'https://wx.idsbllp.cn/MagicLoop/index.php?s=/addon/Api/Api/oauth&redirect=';
-    var nickname;
-    var headimgurl;
-    if((localStorage.getItem('nickname') == undefined) && (localStorage.getItem('headimgurl') == undefined)){
-        window.location = apiUrl + myUrl;
-        nickname = getQueryString(nickname);
-        headimgurl = getQueryString(headimgurl);
-        localStorage.setItem('nickname', nickname);
-        localStorage.setItem('headimgurl', headimgurl);
-    } else {
-        nickname = localStorage.getItem('nickname');
-        headimgurl = localStorage.getItem('headimgurl');
-    }
+
+// function getUserinfo() {
+//     var myUrl = encodeURI('https://wx.idsbllp.cn/nodejs/graduationSeason/');
+//     var apiUrl = 'https://wx.idsbllp.cn/MagicLoop/index.php?s=/addon/Api/Api/oauth&redirect=';
+//     var nickname;
+//     var headimgurl;
+//     if((localStorage.getItem('nickname') == undefined) && (localStorage.getItem('headimgurl') == undefined)){
+//         window.location = apiUrl + myUrl;
+//         nickname = getQueryString(nickname);
+//         headimgurl = getQueryString(headimgurl);
+//         localStorage.setItem('nickname', nickname);
+//         localStorage.setItem('headimgurl', headimgurl);
+//     } else {
+//         nickname = localStorage.getItem('nickname');
+//         headimgurl = localStorage.getItem('headimgurl');
+//     }
     
-}
+// }
 
-function getQueryString(name) {
-    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
-    var r = window.location.search.substr(1).match(reg);
-    if (r != null) {
-        return unescape(r[2]);
-    }
-    return null;
-}
+// function getQueryString(name) {
+//     var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+//     var r = window.location.search.substr(1).match(reg);
+//     if (r != null) {
+//         return unescape(r[2]);
+//     }
+//     return null;
+// }
 
 
 
-window.onload = getUserinfo();
+// window.onload = getUserinfo();
 document.getElementById('username').addEventListener('change', loadUsername)
 document.getElementById('camera_image').addEventListener('change', loadImg)
